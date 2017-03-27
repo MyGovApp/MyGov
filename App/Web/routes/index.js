cf9403fad1f64705a9d501bb3b3e56181e85058b
Ian Lancaster <ianclancaster@gmail.com>
2017-03-26 21:41:26 -0600

    Rename native helloWorld to bills

--- App/Web/routes/index.js
+++ App/Web/routes/index.js
@@ -8,7 +8,6 @@ export const createRoutes = (store) => {
     getChildRoutes (location, next) {
       require.ensure([], (require) => {
         next(null, [
-          require('./HelloWorld').default(store),
           require('./HelloWorldAsync').default(store)
         ])
       })
