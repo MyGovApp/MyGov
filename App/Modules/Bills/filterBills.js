4e5e1fb4cd0cf0ea866d9fbfe7e97868cffd556a
Ian Lancaster <ianclancaster@gmail.com>
2017-04-01 19:53:05 -0600

    Remove bill sort filter caching

--- App/Modules/Bills/filterBills.js
+++ App/Modules/Bills/filterBills.js
@@ -13,16 +13,7 @@ const filterBills = (allBills, options, search) => {
   return sortedBills
 }
 
-const deepEqual = (a, b) => JSON.stringify(a) === JSON.stringify(b)
-
 const filterBillsByStatus = (bills, options) => {
-  const { active, tabled, failed, enacted } = options
-  const params = [ bills, active, tabled, failed, enacted ]
-
-  if (!(active || tabled || failed || enacted)) return bills
-  if (deepEqual(filterBillsByStatus.cachedParams, params)) return filterBillsByStatus.cachedBills
-  filterBillsByStatus.cachedParams = params
-
   let newBills = []
 
   bills.forEach(bill => {
@@ -31,17 +22,11 @@ const filterBillsByStatus = (bills, options) => {
     }
   })
 
-  filterBillsByStatus.cachedBills = newBills
   return newBills
 }
 
 const filterBillsByTopic = (bills, topics) => {
-  const params = [ bills, topics ]
-
   if (!topics.length) return bills
-  if (deepEqual(params, filterBillsByTopic.cachedParams)) return filterBillsByTopic.cachedBills
-  filterBillsByTopic.cachedParams = params
-
   let newBills = []
 
   topics.forEach(topic => {
@@ -56,29 +41,18 @@ const filterBillsByTopic = (bills, topics) => {
     })
   })
 
-  filterBillsByTopic.cachedBills = newBills
   return newBills
 }
 
 const filterBillsBySearch = (bills, search) => {
-  const params = [ bills, search ]
-
-  if (!search.length) return bills
-  if (deepEqual(params, filterBillsBySearch.cachedParams)) return filterBillsBySearch.cachedBills
-  filterBillsBySearch.cachedParams = params
-
-  const newBills = bills.filter((bill) => bill.official_title.toLowerCase().includes(search.toLowerCase()))
-
-  filterBillsBySearch.cachedBills = newBills
+  const newBills = bills.filter((bill) => {
+    return bill.official_title.toLowerCase().includes(search.toLowerCase())
+  })
   return newBills
 }
 
 const sortBills = (bills, sortBy, sortOrder) => {
-  const params = [ bills, sortBy, sortOrder ]
   const compare = (a, b) => sortOrder === 'acending' ? a < b : a > b
-
-  if (deepEqual(params, sortBills.cachedParams)) return sortBills.cachedBills
-  sortBills.cachedParams = params
 
   let newBills = bills
 
@@ -90,11 +64,13 @@ const sortBills = (bills, sortBy, sortOrder) => {
 
   if (sortBy === 'introduced') {
     newBills = bills.sort((billA, billB) => (
-      compare(moment(billA.introduced_on).unix(), moment(billB.introduced_on).unix()) ? -1 : 1
+      compare(
+        moment(billA.introduced_on).unix(),
+        moment(billB.introduced_on).unix()
+      ) ? -1 : 1
     ))
   }
 
-  sortBills.cachedBills = newBills
   return newBills
 }
 
