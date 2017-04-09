f216ba479ee85fa7dc24ebb1eb031b6ae401e8cf
Ian Lancaster <ianclancaster@gmail.com>
2017-04-09 14:20:04 -0600

    Add visual test for untestable billStatusSvg

--- App/Native/Routes/Dev/Dev.component.js
+++ App/Native/Routes/Dev/Dev.component.js
@@ -1,13 +1,56 @@
 import React, { Component, PropTypes } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
+import { BillStatusSvg } from '../../Globals'
 import s from './Dev.styles'
+import snapshots from './snapshots'
+
+const Primary = 'House'
+const Secondary = 'Senate'
+const progress = [
+  `Introduced in ${Primary}`,
+  `${Primary} has taken action`,
+  `Failed in ${Primary}`,
+  `Passed in ${Primary}`,
+  `Failed in ${Secondary}`,
+  `Passed in ${Secondary}`,
+  'Failed in cloture',
+  'Passed in cloture',
+  `Passed  in ${Primary} after cloture`,
+  `Passed in ${Secondary} after cloture`,
+  `Failed in ${Primary} after cloture`,
+  `Failed in ${Secondary} after cloture`,
+  `Passed in ${Primary}, failed in ${Secondary} after cloture`,
+  `Passed in ${Secondary}, failed in ${Primary} after cloture`,
+  `Passed both House and Senate after cloture`,
+  'Vetoed',
+  `Enacted`,
+  `Passed override in ${Primary} after veto`,
+  `Passed override in ${Secondary} after veto`,
+  `Failed override in ${Primary} after veto`,
+  `Failed override in ${Secondary} after veto`,
+  `Passed override in ${Primary}, failed override in ${Secondary} after veto`,
+  `Passed override in ${Secondary}, failed override in ${Primary} after veto`,
+  `Enacted, veto overridden by Congress`
+]
 
 class Dev extends Component {
   render () {
     return (
-      <View style={s.mainContainer}>
-        <Text>Dev Route</Text>
-      </View>
+      <ScrollView style={s.mainContainer}>
+        <Text>Chamber Switch Test</Text>
+        <BillStatusSvg chamber='house' progress={{ index: 5 }} status='active' />
+        <BillStatusSvg chamber='senate' progress={{ index: 5 }} status='active' />
+        <Text>Tabled Check</Text>
+        <BillStatusSvg chamber='house' progress={{ index: 5 }} status='active' />
+        <BillStatusSvg chamber='house' progress={{ index: 5 }} status='tabled' />
+        {progress.map((step, i) =>
+          <View key={i}>
+            <Text>{`${i}: ${step}`}</Text>
+            <Image source={snapshots[i]} />
+            <BillStatusSvg chamber='house' status='active' progress={{ index: i }} />
+          </View>
+        )}
+      </ScrollView>
     )
   }
 }
---
+++ App/Native/Routes/Dev/snapshots/index.js
@@ -0,0 +1,39 @@
+// let snapshots = {}
+//
+// require('./0.png')
+//
+// for (let i = 0; i < 23; i++) {
+//   snapshots = {
+//     ...snapshots,
+//     [i]: require(`./${i}.png`)
+//   }
+// }
+//
+// export default snapshots
+
+export default {
+  0: require('./0.png'),
+  1: require('./1.png'),
+  2: require('./2.png'),
+  3: require('./3.png'),
+  4: require('./4.png'),
+  5: require('./5.png'),
+  6: require('./6.png'),
+  7: require('./7.png'),
+  8: require('./8.png'),
+  9: require('./9.png'),
+  10: require('./10.png'),
+  11: require('./11.png'),
+  12: require('./12.png'),
+  13: require('./13.png'),
+  14: require('./14.png'),
+  15: require('./15.png'),
+  16: require('./16.png'),
+  17: require('./17.png'),
+  18: require('./18.png'),
+  19: require('./19.png'),
+  20: require('./20.png'),
+  21: require('./21.png'),
+  22: require('./22.png'),
+  23: require('./23.png')
+}
