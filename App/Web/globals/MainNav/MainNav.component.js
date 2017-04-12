c425d673ee85e63f2652cb6d034e78ba6f212b7b
Ian Lancaster <ianclancaster@gmail.com>
2017-04-12 16:46:30 -0600

    Update web footer to use shared images

--- App/Web/globals/MainNav/MainNav.component.js
+++ App/Web/globals/MainNav/MainNav.component.js
@@ -2,19 +2,16 @@ import React from 'react'
 import classes from './MainNav.styles.scss'
 import NavIconLink from 'Globals/NavIconLink'
 
-import billsIcon from './assets/bills-icon.png'
-import congressIcon from './assets/congress-icon.svg'
-import accountIcon from './assets/account-icon.svg'
-import notificationsIcon from './assets/notification-icon.svg'
+import I from '../../../Native/Themes/Images'
 
 const MainNav = () => {
   return (
     <nav id={'mainNav'} className={classes.mainNav}>
       <ul>
-        <NavIconLink icon={billsIcon} label={'Bills'} route={'/bills'} />
-        <NavIconLink icon={congressIcon} label={'Congress'} route={'/congress'} />
-        <NavIconLink icon={notificationsIcon} label={'Notifications'} route={'/notifications'} />
-        <NavIconLink icon={accountIcon} label={'Me'} route={'/account'} />
+        <NavIconLink icon={I.billsIcon} label={'Bills'} route={'/bills'} />
+        <NavIconLink icon={I.congressIcon} label={'Congress'} route={'/congress'} />
+        <NavIconLink icon={I.notificationIcon} label={'Notifications'} route={'/notifications'} />
+        <NavIconLink icon={I.accountIcon} label={'Me'} route={'/account'} />
       </ul>
     </nav>
   )
